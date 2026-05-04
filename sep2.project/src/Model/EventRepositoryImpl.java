bee464ecfc6338c42b177e2fec89982a1c59ba53
domi-potrokova <dominikapotrokova@gmail.com>
2026-05-04 17:36:15 +0200

    lalala

--- sep2.project/src/Model/EventRepositoryImpl.java
+++ sep2.project/src/Model/EventRepositoryImpl.java
@@ -3,6 +3,7 @@ package Model;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.DriverManager;
 
 public class EventRepositoryImpl implements EventRepository
 {
